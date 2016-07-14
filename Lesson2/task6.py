1c9d7e6d15bdb269ca8b5dc08cad86f1d5b6dc33
Alexander N. Yatsenko <avatorv@gmail.com>
2016-07-14 09:06:37 -0400

    Added solutions 2.2, 2.3 tasks

--- Lesson2/task6.py
+++ Lesson2/task6.py
@@ -1,22 +1,21 @@
 # coding=utf8
 
-
 # Sixth task definition
-def do_task6():
+def doTask6():
     print '\n* Sixth task started...*\n'
     source_str = 'абвг'
+    source_str_uncode = source_str.decode('utf8')
 
     # Visual checking of input
-    source_str_unicode = str(list(hex(ord(single_char)) for single_char in source_str))
-    print 'Input string: \'%s\', in unicode (to visual checking purposes only): %s' % (source_str, source_str_unicode)
+    print 'Input string: {}.\nInput string unicode: {}\n'.format(repr(source_str), repr(source_str_uncode))
 
     # Calculate character code sum
-    unicode_sum = sum(ord(single_char) for single_char in source_str)
-    print 'Sum of unicode values: %d, in hex: %s' % (unicode_sum, hex(unicode_sum))
+    unicodeSum = sum(ord(c) for c in source_str_uncode)
+    print 'Sum of unicode values: %d' % unicodeSum
 
     print '\n*** Sixth task done...***'
 
 # Auto execute section
 if __name__ == '__main__':
     # Sixth task execution
-    do_task6()
+    doTask6()
--- Lesson2/task7.py
+++ Lesson2/task7.py
@@ -1,12 +1,12 @@
 # Seventh task definition
-def do_task7():
+def doTask7():
     print '\n* Seventh task started...*\n'
     lists = [[1, 2, 3, 4],
              [2, 4, 4, 2],
              [6],
              [1, 1, 1, 1, 1]]
     print 'Source multi-dimension list: ', lists
-    result = map(lambda *e: max(e), *lists)
+    result = map(max, *lists)
     print 'The maximum values from  each list: %s' % result
 
     print '\n*** Seventh task done...***'
@@ -14,5 +14,6 @@ def do_task7():
 # Auto execute section
 if __name__ == '__main__':
     # Seventh task execution
-    do_task7()
+    doTask7()
+
 
